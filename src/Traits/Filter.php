af2fc6b946a23cb76ee832a9dea73d830ac6a256
Gustavo Santarosa <gustavo-computacao@hotmail.com>
2025-03-18 15:40:27 -0300

    Added boolean verification, parameter to ignore params, and enable filters from the request

--- src/Traits/Filter.php
+++ src/Traits/Filter.php
@@ -9,15 +9,27 @@ trait Filter
     protected function filtered(
         string|array|Collection $params,
         ?string $filter = null,
-        string $nameParam = 'filter'
-    ): Collection|false {
+        string $nameParam = 'filter',
+        bool $allowFilter = true,
+        bool $ignoreParams = false,
+        bool $isBoolean = false,
+    ): Collection|bool {
         $collect = match (true) {
             is_string($params)            => $params = collect(explode(',', $params)),
             $params instanceof Collection => $params,
             default                       => $params = collect($params),
         };
 
-        if (!is_null($filter) && (request()->$nameParam[$filter] ?? false)) {
+        if ($ignoreParams) {
+            unset($collect);
+            $collect = collect();
+        }
+
+        if ($allowFilter && !is_null($filter) && (request()->$nameParam[$filter] ?? false)) {
+            if ($isBoolean) {
+                return 'true' === request()->$nameParam[$filter] ? true : false;
+            }
+
             collect(explode(',', request()->$nameParam[$filter]))->each(function ($id) use (&$collect) {
                 $collect[] = $id;
             });
